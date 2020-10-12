1ab0e8cedcfadd18fe13647600ea821d177cdbde
髙藏一平 <takakurapei1@gmail.com>
2020-10-12 23:25:57 +0900

    no message

--- yugotube/lib/main.dart
+++ yugotube/lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Generated App',
-      theme: new ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.red,
         primaryColor: Colors.red,
         accentColor: Colors.red,
         canvasColor: Colors.black,
       ),
-      home: new MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -23,35 +23,38 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Yugotube'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Yugotube'),
       ),
       body:
-      new Column(
+      Column(
           mainAxisAlignment: MainAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            new Image.network(
+             Image.network(
               'https://th.bing.com/th/id/OIP.9ePs7-J5lwAgs4144o7ToQHaEK?w=316&h=180&c=7&o=5&dpr=1.2&pid=1.7',
               fit: BoxFit.fill,
             ),
-
-            new TextField(
-              style: new TextStyle(fontSize: 24.0,
+             Container(
+               decoration: BoxDecoration(
+                 border: Border.all(color: Colors.white, width: 2),
+               )
+             ),
+             TextField(
+              style: TextStyle(fontSize: 24.0,
                   color: Colors.white,
                   fontWeight: FontWeight.w400,
                   fontFamily: "Roboto"),
             ),
           ]
       ),
-
     );
   }}
